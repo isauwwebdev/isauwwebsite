85c5c3b8c215b0d6a4205838d537a3c3b1291958
Andrew Darmawan <andrew.d.tjah@gmail.com>
2022-09-18 18:07:57 +0700

    feat: added some styling and css require review

--- src/components/Apply.jsx
+++ src/components/Apply.jsx
@@ -1,22 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 // import FileUploaded from './FileUploaded';
 import Carousel from 'react-bootstrap/Carousel';
 
 
 function Apply() {
-// gdrive client ID: 799175424998-b3j32lo9hpli5lkc3e886d9lft6vicmg.apps.googleusercontent.com
-// gdrive client secret: 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
+  // gdrive client ID: 799175424998-b3j32lo9hpli5lkc3e886d9lft6vicmg.apps.googleusercontent.com
+  // gdrive client secret: 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
   const [info, setInfo] = useState({
-      name: "",
-      phoneNumber: "",
-      email: "",
-      major: "",
-      year: "",
-      strengths: "",
-      past: "",
-      c1:"",
-      c2:"",
-      c3:"",
+    name: "",
+    phoneNumber: "",
+    personal_email: "",
+    uw_email: "",
+    major: "",
+    year: "",
+    strengths: "",
+    past: "",
+    why: "",
+    c1: "",
+    c2: "",
+    c3: "",
   });
 
   // const CLIENT_ID = '799175424998-b3j32lo9hpli5lkc3e886d9lft6vicmg.apps.googleusercontent.com';
@@ -34,22 +36,17 @@ function Apply() {
   // oauth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
 
   // need time to upload resume, need to delay their responses
-  const[resume, setResume] = useState(null);
-  const[link, setLink] = useState("");
+  const [resume, setResume] = useState(null);
+  const [link, setLink] = useState("");
 
-  const {name, phoneNumber, email, major, year, strengths, past, c1, c2, c3} = info;
+  const { name, phoneNumber, personal_email, uw_email, major, year, strengths, past, why, c1, c2, c3 } = info;
 
   const handleChange = (event) => {
-      setInfo({ ...info, [event.target.name]: event.target.value });
+    setInfo({ ...info, [event.target.name]: event.target.value });
   }
-
-const delay = ms => new Promise(
-  resolve => setTimeout(resolve, ms)
-);
-
-
-const handleFile = (e) =>{
-  const fileObj = e.target.files && e.target.files[0];
+  
+  const handleFile = (e) => {
+    const fileObj = e.target.files && e.target.files[0];
     if (!fileObj) {
       return;
     }
@@ -58,115 +55,160 @@ const handleFile = (e) =>{
     setResume(fileObj)
     console.log(e.target.files[0])
     uploadFile(e);
-}
+  }
 
-    const uploadFile = (e) => {
-      let file = resume
-      let reader = new FileReader()
-      try{
-        reader.readAsDataURL(file)
-      }catch(err){
-        alert("file failed to upload")
-        e.target.value = null;
-      }
+  const uploadFile = (e) => {
+    let file = resume
+    let reader = new FileReader()
+    try {
+      reader.readAsDataURL(file)
+    } catch (err) {
+      alert("file failed to upload")
+      e.target.value = null;
+    }
 
-      reader.onload = function(e){
-        let rawLog = reader.result.split(',')[1];
-        var dataSend = { dataReq: { data: rawLog, name: file.name, type: file.type }, fname: "uploadFilesToGoogleDrive" }; //preapre info to send to API
-          fetch('https://script.google.com/macros/s/AKfycbzS7gM0878oabxpLc1eB442C90L3-DJANDxDIpVD3w77i9oLtPfJxcyG9vjwoP_gr8T/exec', //your AppsScript URL
-            { method: "POST", body: JSON.stringify(dataSend) }) //send to Api
-            .then(res => res.json()).then((a) => {
-              console.log(a.url) //See response
-              setLink(a.url)
-            }).catch(e => console.log(e)) // Or Error in console
-        }
-      };
+    reader.onload = function (e) {
+      let rawLog = reader.result.split(',')[1];
+      var dataSend = { dataReq: { data: rawLog, name: file.name, type: file.type }, fname: "uploadFilesToGoogleDrive" }; //preapre info to send to API
+      fetch('https://script.google.com/macros/s/AKfycbzS7gM0878oabxpLc1eB442C90L3-DJANDxDIpVD3w77i9oLtPfJxcyG9vjwoP_gr8T/exec', //your AppsScript URL
+        { method: "POST", body: JSON.stringify(dataSend) }) //send to Api
+        .then(res => res.json()).then((a) => {
+          console.log(a.url) //See response
+          setLink(a.url)
+        }).catch(e => console.log(e)) // Or Error in console
+    }
+  };
 
-const submitForm = async (e) => {
-  delay(10000);
-};
+  const submitForm = async (e) => {
+    setTimeout( function () { 
+      e.submit();
+  }, 30000);
+  };
 
   return (
     <div>
-    <div className="navbar-overlay" style={{position: "relative"}}>
-      <img
-        className="d-block w-100"
-        src="../images/officers/isauw-group.jpg"
-        alt=""
-        style={{height: "calc(50vh + 10vw)"}}
-      />
-      <Carousel.Caption style={{top: "40%", bottom: "60%"}} className="animated fadeInUp">
-        <h1 className="carousel-title" style={{zIndex: "100", fontFamily: "brandon_grotesque, sans-serif", fontWeight: "400", color:"red"}}>Your Journey at ISAUW starts here!!!</h1>
-      </Carousel.Caption>    </div>
+      <div className="navbar-overlay" style={{ position: "relative" }}>
+        <img
+          className="d-block w-100"
+          src="../images/officers/isauw-group.jpg"
+          alt=""
+          style={{ height: "calc(50vh + 10vw)" }}
+        />
+        <Carousel.Caption style={{ top: "40%", bottom: "60%" }} className="animated fadeInUp">
+          <h1 className="carousel-title" style={{ zIndex: "100", fontFamily: "brandon_grotesque, sans-serif", fontWeight: "400", color: "red" }}>
+            Your Journey at ISAUW starts here!!!</h1>
+        </Carousel.Caption>
+      </div>
 
+      <h1 className="apply-title"> Apply form </h1>
+      <h6>ISAUW Officer Recruitment 22'-23'</h6>
+      <h6>We are thrilled to welcome new members to ISAUW!</h6>
+      <h6>We are currently recruiting enthusiastic, driven and collaborative students in the UW community to join our 2022-2023 cabinet.</h6>
+      <h6>Please fill in this form to apply, qualified students will be contacted for an interview.</h6>
+      <h6>Do not hesitate to contact us on Instagram @isauwhuskies or email us at isauw@uw.edu if you have any questions.</h6>
+      <h6>Requirement: currently enrolled as a UW student.</h6>
       <form
-      className="apply-form"
-      method="POST"
-      action="https://script.google.com/macros/s/AKfycbzCwqJl0_tfZrPQsVyYmCWfjmpfLwXkJwK9VW4ihZBmIGoZnWv01nais7SNnWeKya4/exec"
+        className="apply-form"
+        method="POST"
+        action="https://script.google.com/macros/s/AKfycbzCwqJl0_tfZrPQsVyYmCWfjmpfLwXkJwK9VW4ihZBmIGoZnWv01nais7SNnWeKya4/exec"
       >
-        <h3> Apply form </h3>
-        <label>
-          Full Name:
-          <input
-            type="text"
-            name="name"
-            value={name}
-            onChange={handleChange}
-            autoComplete="on"
-            required
-           />
-        </label>
-        <br/>
-        <label>
-          Phone:
-          <input
-            type="number"
-            name="phoneNumber"
-            value={phoneNumber}
-            international = {false}
-            onChange={handleChange}
-            autoComplete="on"
-            defaultCountry={'US'}
-            countries={["NG", "MG", "SC", "KM", "BW", "MR"]}
-            required
-           />
-        </label>
-        <br/>
-        <label>
-          Email:
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={handleChange}
-            autoComplete="on"
-            required
-           />
-        </label>
+        <span class="input"></span>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          placeholder="Full name"
+          onChange={handleChange}
+          autocomplete="off"
+          title="Format: Xx[space]Xx (e.g. Alex Cican)"
+          autofocus
+          required
+          pattern="^\w+\s\w+$"
+        />
         <br />
-        <label>
-          Major:
-          <input
-            type="text"
-            name="major"
-            value={major}
-            onChange={handleChange}
-            autoComplete="on"
-            required
-           />
-        </label>
+        <input
+          type="email"
+          name="personal_email"
+          value={personal_email}
+          onChange={handleChange}
+          autocomplete="off"
+          placeholder="Personal Email"
+          required
+        />
         <br />
-        <label>
-        Current Year:
-          <select name="year" value={year} onChange={handleChange}>
-            <option value="freshman">Freshman</option>
-            <option value="sophomore">Sophomore</option>
-            <option value="junior">Junior</option>
-            <option value="senior">Senior</option>
-          </select>
-        </label>
+        <input
+          type="email"
+          name="uw_email"
+          value={uw_email}
+          onChange={handleChange}
+          autocomplete="on"
+          placeholder="UW Email"
+          title="Format: xxx@uw.edu"
+          autofocus
+          required
+          pattern="^\w+@uw.edu"
+        />
         <br />
-          From most to least priority, list up to 3 positions that you are most interested in and explain why.
+        <span class="input"></span>
+        <input
+          type="number"
+          name="phoneNumber"
+          value={phoneNumber}
+          international={false}
+          onChange={handleChange}
+          autocomplete="on"
+          defaultCountry={'US'}
+          countries={["NG", "MG", "SC", "KM", "BW", "MR"]}
+          placeholder="WA Phone Number"
+          required
+        />
+        <br />
+        <input
+          type="text"
+          name="major"
+          value={major}
+          onChange={handleChange}
+          autocomplete="on"
+          placeholder="Major/Intended Major"
+          required
+        />
+        <br />
+        <select className="curr_year" name="year" value={year} onChange={handleChange} required>
+          <option disabled selected value="">  Current Year</option>
+          <option value="freshman">Freshman</option>
+          <option value="sophomore">Sophomore</option>
+          <option value="junior">Junior</option>
+          <option value="senior">Senior</option>
+          <option value="graduate">Graduate</option>
+        </select>
+        <br />
+        <ul className='positions'>ISAUW officer position options:
+
+          <li>Event Organizer</li>
+          <li>Creativity Management</li>
+          <li>Inventory</li>
+          <br />
+          <li>Sponsorship</li>
+          <li>Fundraising</li>
+          <li>Treasury</li>
+          <br />
+          <li>Marketing Communication</li>
+          <li>Design</li>
+          <li>Documentation</li>
+          <li>Web Development</li>
+          Want to know more about the positions? <a href='https://docs.google.com/presentation/d/1WZyhpHxiMuP-IsPmlmj5wFNDYkGTLTTxwPLy_uXqvLE/edit?usp=sharing'>Roles of Each Position</a>
+        </ul>
+        <br />
+
+        <h5>Please rank the positions you are interested in (1 being the most)</h5>
+        <h5>*For Design, Documentation, and Creativity Management Officers, please attach a portfolio/collection of your work and bring a hard/soft copy to the interview.</h5>
+        <h5>*For all officers, please bring a hard/soft copy of your Resume if you have one.</h5>
+
+        <br />
+        <p>Resume:</p>
+        <input type="file" name="resume" className='resume_class' required onChange={(e) => {handleFile(e)}}/>
+        <br />
+        <p>From most to least priority, list up to 3 positions that you are most interested in and explain why.</p>
         <br />
         choice 1:
         <br />
@@ -186,22 +228,22 @@ const submitForm = async (e) => {
         choice 2:
         <br />
         <label>
-        <select name="c2" value={c2} onChange={handleChange}>
-          <option value="EO">Event Organizers</option>
-          <option value="IO">Inventory & Logistics</option>
-          <option value="CM">Creativity Management</option>
-          <option value="Sponsor">Sponsorship</option>
-          <option value="Treasury">Treasury</option>
-          <option value="MarCom">Marketing Communication</option>
-          <option value="Documentary">Documentary and Design*</option>
-          <option value="IT">Information Technology</option>
-        </select>
+          <select name="c2" value={c2} onChange={handleChange}>
+            <option value="EO">Event Organizers</option>
+            <option value="IO">Inventory & Logistics</option>
+            <option value="CM">Creativity Management</option>
+            <option value="Sponsor">Sponsorship</option>
+            <option value="Treasury">Treasury</option>
+            <option value="MarCom">Marketing Communication</option>
+            <option value="Documentary">Documentary and Design*</option>
+            <option value="IT">Information Technology</option>
+          </select>
         </label>
         <br />
         choice 3:
         <br />
 
-          <label>
+        <label>
           <select name="c3" value={c3} onChange={handleChange}>
             <option value="EO">Event Organizers</option>
             <option value="IO">Inventory & Logistics</option>
@@ -212,45 +254,53 @@ const submitForm = async (e) => {
             <option value="Documentary">Documentary and Design*</option>
             <option value="IT">Information Technology</option>
           </select>
-          </label>
-          <br />
-          <b>* bring portfolio.</b>
-          <br />
-          Resume:
-          <br />
-          <input type="file" name="resume" onChange={(e)=>{
-            handleFile(e)
-          }
-        }/>
-        <br/>
+        </label>
+        <br />
+        <b>* bring portfolio.</b>
+        <br />
+        <h4>Tell us a bit about yourself!</h4>
         <label>
-          What would you say are your strengths and weaknesses?
+          What would you say are your strengths and weaknesses? (2 each) and why?
           <br />
           <textarea
-          type="text"
-          name="strengths"
-          value={strengths}
-          onChange={handleChange}
-          required
+            className='strengths_weaknesses'
+            type="text"
+            name="strengths"
+            value={strengths}
+            onChange={handleChange}
+            required
           />
         </label>
         <br />
         <label>
-          Describe a past experience that you think will help you contribute to ISAUW.
+          What past experiences have you had that would be an asset for ISAUW?
           <br />
           <textarea
-          type="text"
-          name="past"
-          value={past}
-          onChange={handleChange}
-          required
+            className="past_experience"
+            type="text"
+            name="past"
+            value={past}
+            onChange={handleChange}
+            required
           />
         </label>
         <br />
-
-        <input type="hidden" name="link"  value={link}/>
+        <label>
+          Why do you want to join ISAUW? (brief description)
           <br />
-          <button onClick={submitForm}>Submit</button>
+          <textarea
+            className='why_isauw'
+            type="text"
+            name="why"
+            value={why}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <input type="hidden" name="link" value={link} />
+        <br />
+        <button onClick={submitForm}>Submit</button>
       </form>
     </div>
   );
