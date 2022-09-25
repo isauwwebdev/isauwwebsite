80a01c2a458f1be3c5cb3a6b244dcf5760e38acc
Audrey <yipaud03@uw.edu>
2022-09-24 17:23:50 -0700

    fix: have navbar appear

--- src/components/Home/NotFoundPage.jsx
+++ src/components/Home/NotFoundPage.jsx
@@ -3,14 +3,14 @@ import Carousel from 'react-bootstrap/Carousel';
 
 function NotFoundPage() {
     return (
-      <div className="navSolid" style={{position: "relative"}}>
+      <div className="navSolid2" style={{position: "relative"}}>
          <img
            className="not-found-page-image"
            src="../images/page-not-found.png"
            alt=""
            style={{height: "calc(40vh + 10vw)"}}
          />
-      <Carousel.Caption style={{top: "100%", bottom: "65%", left: "5%", right: "5%"}}>
+      <Carousel.Caption style={{top: "75%", bottom: "65%", left: "5%", right: "5%"}}>
         <h1 className="not-found-page" style={{zIndex: "100"}}>404 Page Not Found</h1>
       </Carousel.Caption>
     </div>
