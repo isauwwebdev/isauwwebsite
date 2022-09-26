a35c01e62b397a7ffb7e21516693a804f3fc78ac
Audrey <yipaud03@uw.edu>
2022-09-25 22:19:49 -0700

    fix: 404 in own line

--- src/components/Home/NotFoundPage.jsx
+++ src/components/Home/NotFoundPage.jsx
@@ -3,15 +3,16 @@ import Carousel from 'react-bootstrap/Carousel';
 
 function NotFoundPage() {
     return (
-      <div className="navSolid2" style={{position: "relative"}}>
+      <div className="navHide" style={{position: "relative"}}>
          <img
            className="not-found-page-image"
            src="../images/page-not-found.png"
            alt=""
            style={{height: "calc(40vh + 10vw)"}}
          />
-      <Carousel.Caption style={{top: "75%", bottom: "65%", left: "5%", right: "5%"}}>
-        <h1 className="not-found-page" style={{zIndex: "100"}}>404 Page Not Found</h1>
+      <Carousel.Caption style={{top: "85%", bottom: "65%", left: "5%", right: "5%"}}>
+        <h1 className="not-found-page" style={{zIndex: "100", fontWeight: "bolder"}}>404</h1>
+        <h1 className="not-found-page" style={{zIndex: "100"}}>Page Not Found</h1>
       </Carousel.Caption>
     </div>
     );
