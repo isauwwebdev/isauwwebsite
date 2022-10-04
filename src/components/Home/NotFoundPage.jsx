493f605175d717d15725330103c7588a64749722
Audrey <yipaud03@uw.edu>
2022-10-04 00:11:53 -0700

    fix: from feedback

--- src/components/Home/NotFoundPage.jsx
+++ src/components/Home/NotFoundPage.jsx
@@ -8,10 +8,11 @@ function NotFoundPage() {
            className="not-found-page-image"
            src="../images/page-not-found.png"
            alt=""
-           style={{height: "calc(40vh + 10vw)"}}
+           style={{height: "calc(60vh + 10vw)"}}
          />
       <Carousel.Caption>
-        <h1 className="not-found-page" style={{fontWeight: "bold", fontSize: "calc(2vw + 30px)"}}>404</h1>
+      <br></br>
+        <h1 className="not-found-page" style={{fontWeight: "bold", fontSize: "calc(1vw + 100px)", marginTop: "5rem", paddingTop: "1rem"}}>404</h1>
         <h1 className="not-found-page">Page Not Found</h1>
       </Carousel.Caption>
     </div>
