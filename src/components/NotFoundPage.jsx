2aaee61ab87e3eead87fe4ba47f094adce034cc2
Audrey <yipaud03@uw.edu>
2022-10-19 12:57:04 -0700

    feat: 404 page

---
+++ src/components/NotFoundPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function NotFoundPage() {
+    return (
+    <body style={{height:"100vh"}}>
+        <div id="notfound">
+            <div class="notfound">
+                <div class="notfound-404">
+                    <h1>404</h1>
+                </div>
+                <h2>PAGE NOT FOUND</h2>
+                <br></br>
+                <a href="/">Go To Homepage</a>
+            </div>
+        </div>
+    </body>
+    );
+}
+
+export default NotFoundPage;
