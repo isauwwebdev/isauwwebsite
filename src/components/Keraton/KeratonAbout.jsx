94ad6a4d9d25dbe13ecff627b6396d09cbbca1c1
dgunadharma20 <dgunadharma20@gmail.com>
2023-04-30 19:07:34 -0700

    fix: student performers design

--- src/components/Keraton/KeratonAbout.jsx
+++ src/components/Keraton/KeratonAbout.jsx
@@ -13,12 +13,12 @@ function KeratonAbout() {
         <Row className="keraton-about-row">
           <Col className="my-auto" xs={12} sm={12} md={6} lg={6} style={{ padding: "0" }}>
 
-            <p className="keraton-about-text mx-4" style={{ fontSize: `calc(12px + 0.4vw)` }}>
+            <p className="keraton-about-text mx-4 my-auto" style={{ fontSize: `calc(12px + 0.4vw)` }}>
               Keraton is an Indonesian Festival and ISAUW’s most iconic event of the year. Currently, Keraton is the largest Indonesian Festival on the West Coast and the second largest in the United States. This event is held annually at the University of Washington since 2011.
             </p>
           </Col>
           <Col xs={12} sm={12} md={6} lg={6} style={{ display: "flex", alignItems: "center", padding: "0" }}>
-            <div className="keraton-countdown-container mx-auto mt-5">
+            <div className="keraton-countdown-container mx-auto my-auto">
               <KeratonCountdown keratonDate={new Date("2023-05-06")}></KeratonCountdown>
             </div>
           </Col>
