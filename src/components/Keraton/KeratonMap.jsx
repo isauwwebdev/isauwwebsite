a22ee24c97b305181f0d9ca108df7e9288ca4704
dgunadharma20 <dgunadharma20@gmail.com>
2023-05-02 01:32:03 -0700

    fix: margin/padding fixes for each section

--- src/components/Keraton/KeratonMap.jsx
+++ src/components/Keraton/KeratonMap.jsx
@@ -6,7 +6,7 @@ import Container from 'react-bootstrap/Container';
 function KeratonMap() {
     return (
         <div className="py-1" style={{}}>
-            <Container className="keraton-map mx-auto" style={{ paddingTop: "3rem", paddingBottom: "7rem" }}>
+            <Container className="keraton-map mx-auto" style={{ paddingTop: "3rem", paddingBottom: "2rem" }}>
                 <h1 className="py-2 mb-3 keraton-section-header">Map</h1>
                 <img src="../../images/keraton/2023/map.png"></img>
             </Container>
