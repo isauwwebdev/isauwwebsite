c6d99971b27f056e857c9694eb07c8726c9d3f10
dgunadharma20 <dgunadharma20@gmail.com>
2023-05-01 22:47:37 -0700

    done with draft

---
+++ src/components/Keraton/KeratonMap.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+
+function KeratonMap() {
+    return (
+        <div className="py-1" style={{}}>
+            <Container className="keraton-map mx-auto" style={{ paddingTop: "3rem", paddingBottom: "7rem" }}>
+                <h1 className="py-2 mb-3 keraton-section-header">Map</h1>
+                <img src="../../images/keraton/2023/map.png"></img>
+            </Container>
+        </div>
+    )
+}
+
+export default KeratonMap;
