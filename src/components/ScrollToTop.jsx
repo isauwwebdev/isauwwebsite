d8134f14ff7973ebc913eb947c89b9e762cdd1fd
Arvin Oentoro <arvinj@cs.washington.edu>
2023-08-28 16:36:32 -0700

    ScrollToTop: added comments for clarity

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -4,6 +4,7 @@ import {useLocation} from "react-router-dom";
 export default function ScrollToTop() {
   const {pathname} = useLocation();
 
+  // scrolls to the top when a different page (or UI) is loaded
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
