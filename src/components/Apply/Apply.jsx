68bb0e55c0b60c4e95b1060b691d650b73dc6c58
Arvin Oentoro <arvinj@cs.washington.edu>
2023-09-20 05:49:18 -0700

    request mode: no cors

--- src/components/Apply/Apply.jsx
+++ src/components/Apply/Apply.jsx
@@ -139,10 +139,7 @@ function Apply() {
       };
 
       // upload to Google Drive
-      fetch('https://script.google.com/a/macros/uw.edu/s/AKfycbxaFfxMyInXQ5uxRwgQIgyu0OWlKdCiUqSCZW550AB1Cmh8prze0sg3marbAfmqTDiA/exec', {method: 'POST', body: JSON.stringify(dataSend), 
-                                                                                                                                                          headers: {
-                                                                                                                                                            "Origin" : "https://script.google.com/a/macros/uw.edu/s/AKfycbxaFfxMyInXQ5uxRwgQIgyu0OWlKdCiUqSCZW550AB1Cmh8prze0sg3marbAfmqTDiA/exec"
-                                                                                                                                                          }})
+      fetch('https://script.google.com/a/macros/uw.edu/s/AKfycbxaFfxMyInXQ5uxRwgQIgyu0OWlKdCiUqSCZW550AB1Cmh8prze0sg3marbAfmqTDiA/exec', {method: 'POST', body: JSON.stringify(dataSend), mode: 'no-cors'})
         .then(res => res.json()).then((a) => {
           console.log(a);
           formData.append('resume', a.url);
