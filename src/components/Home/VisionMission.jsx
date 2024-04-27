7d9e3f194088b5279b8993ea84c20cfb41bf98a3
Arvin Oentoro <arvinj@cs.washington.edu>
2024-04-27 14:15:32 -0700

    remove keraton website temp

--- src/components/Home/VisionMission.jsx
+++ src/components/Home/VisionMission.jsx
@@ -1,24 +1,46 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
+import Container from 'react-bootstrap/Container'
+
+const visBorder = {
+  left: "100px",
+  border: "solid",
+  borderRadius: "20px",
+  innerWidth: "20px",
+  marginLeft: "20px",
+  marginRight: "20px"
+}
+
+const misBorder = {
+  left: "100px",
+  border: "solid",
+  borderRadius: "20px",
+  innerWidth: "20px",
+  marginLeft: "20px",
+  marginRight: "20px",
+  boxShadow: "10px 5px 5px grey"
+}
 
 function VisionMission() {
   return (
-    <section className="my-5 mx-auto" style={{width: "70%"}}>
+    <section className="my-5 mx-auto" style={{width: "90%"}}>
       <Fade bottom cascade>
-        <div className="row justify-content-md-center">
-          <div className="col-md-8 text-center" style={{left: "100px"}}>
-            <h1 className="my-3"><strong>Our Vision</strong></h1>
-            <p className="my-3">
-              To celebrate Indonesia's diverse culture by fostering a place of belonging and engaging in activities that build understanding and advocacy.
-            </p>
+        <Container style={{position: "relative"}}>
+          <div className="row justify-content-md-center width">
+            <div className="col-sm text-center mr-4" style={visBorder}>
+              <h1 className="my-3"><strong>Our Vision</strong></h1>
+              <p className="my-3">
+                To celebrate Indonesia's diverse culture by fostering a place of belonging and engaging in activities that build understanding and advocacy.
+              </p>
+            </div>
+            <div className="col-sm text-center mr-4" style={misBorder}>
+              <h1 className="my-3"><strong>Our Mission</strong></h1>
+              <p className="my-3">
+                To become a leading organization of proud Indonesian students that promotes cultural diversity in the Greater Seattle Area and beyond.
+              </p>
+            </div>
           </div>
-          <div className="col-md-8 text-center">
-            <h1 className="my-3"><strong>Our Mission</strong></h1>
-            <p className="my-3">
-              To become a leading organization of proud Indonesian students that promotes cultural diversity in the Greater Seattle Area and beyond.
-            </p>
-          </div>
-        </div>
+        </Container>
       </Fade>
     </section>
   );
